a2314ffcaccf90e32c47fad346224aaa59a217ac
ConorMB93 <Conor.dit93@gmail.com>
2016-12-23 21:20:20 +0000

    Some new error handling

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, request
 import re
+
+from flask import flash
 from textblob import TextBlob
+from flask import redirect, url_for
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = "C:\\Users\conor\PycharmProjects\SentimentWebApp"
+ALLOWED_EXTENSIONS = set(['txt'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 class WhatsApp:
@@ -27,13 +36,49 @@ def main():
 
 @app.route('/upload/', methods=['GET', 'POST'])
 def upload():
-    sentiment_value = "Something ... "
+
     if request.method == 'POST':
-        f = request.files['file']
-        text = f.filename
-        m_list = split_at_regex(text)
-        sentiment_value = sentiment_eval(m_list)
-    return render_template("complete.html", sentiment_value=sentiment_value)
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        #print("A")
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            text = read_file()
+            m_list = split_at_regex(text)
+            sentiment_value = sentiment_eval(m_list)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            return render_template("complete.html", sentiment_value=sentiment_value)
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    print(text)
+
+    return text
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+def read_file():
+    file = open(UPLOAD_FOLDER + "/" + "WhatsApp_Chat_with_Connie_3.txt", encoding="utf8")
+    text = file.read()
+    print(text)
+
+    return text
 
 
 def split_at_regex(text):
@@ -49,18 +94,31 @@ def split_at_regex(text):
         whats_app = WhatsApp(name, message)
         whats_app_list.append(whats_app)
 
+        #print("name: " + str(name))
+        #print("message: " + str(message))
+
     return whats_app_list
 
 
 def sentiment_eval(m_list1):
+
+    name_to_analyze = my_form_post()
+    print(name_to_analyze)
     text = ""
+    error_message = "The name you entered was not found :/"
 
     for o in m_list1:
-        if o.get_name() == "Conor Byrne":
+        if o.get_name() == name_to_analyze:
             text = text + o.get_message()
-    tb1 = TextBlob(text)
-    value = tb1.sentiment
+            print(text)
 
+    if text == "":
+        value = error_message
+        print("X")
+    else:
+        tb1 = TextBlob(text)
+        value = tb1.sentiment
+        print("Y")
     return value
 
 
